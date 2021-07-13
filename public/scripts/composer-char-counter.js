ce1cf64f9066bd3d4ec562c5ada36d1607161aab
maggiezhao11 <maggiezhao06@hotmail.com>
2021-07-13 18:56:33 +0000

    added color changed on negative counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
-  $("#tweet-text").on('input', function() {
+  $("#tweet-text").on('change', function() {
     const length = $(this).val().length;
     const counterNum = $(this).closest("#userInput").find(".counter")
     const newCounter = 140 - length;
     counterNum.text(newCounter);  
-    });
+    if ( (newCounter) < 0 ) {
+      $counterNum.addClass("colorRed");
+    } else {
+      $counterNum.removeClass("colorRed");
+    }
+});
+
 });
 
 
