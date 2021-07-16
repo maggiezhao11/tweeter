f46f0d9a5b16d03faab622693ad1af8e6a14c1a4
maggiezhao11 <maggiezhao06@hotmail.com>
2021-07-16 21:26:05 +0000

    added toggle 2 and nav arrow animation

--- public/scripts/composer-button.js
+++ public/scripts/composer-button.js
@@ -4,3 +4,20 @@ $(document).ready(function() {
     $("#tweet-text").focus();
   });
 });
+
+$(document).ready(function() {
+  $("#back-top").hide();
+  $(window).on("scroll", function() {
+    let showAfter = 100;
+    if ( $(this).scrollTop() > showAfter ) {
+      $("#back-top").show();
+    } else {
+      $("#back-top").hide();
+    }
+  });
+  $("#back-top").on("click", function() {
+    $(window).scrollTop();
+    console.log("line19")
+    $("#tweet-text").focus();
+  });
+});
